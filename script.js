921af2300fb51aa32ff2b195f9eb250486792ab1
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-04-02 00:23:10 +0600

    All done project complete

--- script.js
+++ script.js
@@ -15,19 +15,72 @@ barBtn.addEventListener("click", () => {
   }
 });
 
-//Navigation bar dropdown toggle function  ends
+// Navigation bar dropdown toggle function  ends
 
-//Fruit moving on mouse move
+// Fruit moving on mouse move
 
-//variables
+// variables
 
-// let fruit = document.querySelectorAll("img");
-// let heroContainer = document.querySelector(".hero-container");
+let fruit = document.querySelectorAll(".img");
+let heroContainer = document.querySelector(".hero-container");
+let last_section = document.querySelector(".last");
 
-// heroContainer.addEventListener("mousemove", (e) => {
-//   fruit.forEach((el) => {
-//     let pageX = e.pageX / 100 + "px";
-//     let pageY = e.pageY / 100 + "px";
-//     el.style.transform = `translate(${pageX} ${pageY})`;
-//   });
-// });
+// function hero-section
+
+heroContainer.addEventListener("mousemove", (e) => {
+  fruit.forEach((el) => {
+    let pageX = e.pageX / 20 + "px";
+    let pageY = e.pageY / 20 + "px";
+    el.style.transform = `translate(${pageX}, ${pageY})`;
+  });
+});
+
+// function hero-section
+
+last_section.addEventListener("mousemove", (e) => {
+  fruit.forEach((el) => {
+    let pageX = e.pageX / 20 + "px";
+    let pageY = e.pageY / 20 + "px";
+    el.style.transform = `translate(${pageX}, ${pageY})`;
+  });
+});
+
+//Intersection Observer
+
+//bottom
+let Observer = new IntersectionObserver(
+  (el) => {
+    el.forEach((el) => {
+      if (el.isIntersecting) {
+        el.target.classList.add("bottom-show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+let bottom_hidden = document.querySelectorAll(".bottom");
+bottom_hidden.forEach((el) => {
+  Observer.observe(el);
+});
+
+//right
+let rigthObserver = new IntersectionObserver(
+  (el) => {
+    el.forEach((el) => {
+      if (el.isIntersecting) {
+        el.target.classList.add("right-show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+let right_hidden = document.querySelectorAll(".right");
+right_hidden.forEach((el) => {
+  rigthObserver.observe(el);
+});
